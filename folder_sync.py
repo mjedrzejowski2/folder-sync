930c0488238fc16a48ba7fbef86fed768db5944b
mjedrzejowski2 <mjedrzejowski00@gmail.com>
2025-06-22 18:09:46 +0200

    Add main logic - sync files and compare them based on sha256

--- folder_sync.py
+++ folder_sync.py
@@ -7,9 +7,13 @@
 #args: path to source folder, path to replica folder, interval between syncs, amount of sync, path to log file
 
 #Initial logger
+import os
 import logging
 import json
 import argparse
+import shutil
+import hashlib
+from time import time
 from dataclasses import dataclass
 
 
@@ -84,6 +88,8 @@ class Config:
 
 
 class SynchronizeFiles:
+    #przejsc do folderu source_folder_path i sprawdzic czy wszystkie pliki sa na miejsu, to znaczy
+    # sprawdzic czy sa nowe lub stare pliki, jezeli tak to zaupdatowac, a te co sa to policzyc md5
     def __init__(self, sync_interval, sync_numbers, source_folder_path, replica_folder_path):
         self.sync_interval = sync_interval
         self.sync_numbers = sync_numbers
@@ -95,7 +101,60 @@ class SynchronizeFiles:
 
     def run(self):
         for i in range(self.sync_numbers):
-            pass 
+            self._sync_once()
+
+            if i < self.sync_numbers - 1: # prevent from sleeping at last iteration 
+                time.sleep(self.sync_interval)
+
+    def _sync_once(self):
+        self._check_and_sync()
+        self._remove_extras()
+    
+    def _check_and_sync(self):
+        for root, dirs, files in os.walk(self.source_folder_path):
+            source_rel_path = os.path.relpath(root, self.source_folder_path)
+            replica_root_path = os.path.join(self.replica_folder_path, source_rel_path)
+
+            if not os.path.exists(replica_root_path):
+                os.makedirs(replica_root_path)
+            
+            for file in files:
+                source_file_path = os.path.join(root, file)
+                replica_file_path = os.path.join(replica_root_path, file)
+
+                if not os.path.exists(replica_file_path) or self._file_changed(source_file_path, replica_file_path):
+                    shutil.copy2(source_file_path, replica_file_path)
+
+    def _remove_extras(self):
+        for root, dirs, files in os.walk(self.source_folder_path, topdown=False):
+            source_rel_path = os.path.relpath(root, self.source_folder_path)
+            replica_root_path = os.path.join(self.replica_folder_path, source_rel_path)
+            
+            for file in files:
+                source_file_path = os.path.join(root, file)
+                replica_file_path = os.path.join(replica_root_path, file)
+            
+                if not os.path.exists(source_file_path):
+                    os.remove(replica_file_path)
+            
+            for dir in dirs:
+                replica_dir_path = os.path.join(root, dir)
+                source_dir_path = os.path.join(source_rel_path)
+
+                if not os.path.exists(source_dir_path):
+                    shutil.rmtree(replica_dir_path)
+
+
+    def _sha256_check(self, file_path):
+        hash_sha256 = hashlib.sha256()
+        with open(file_path, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hash_sha256.update(chunk)
+        
+        return hash_sha256.hexdigest()
+    
+    def _file_changed(self, file_1, file_2):
+        return self._sha256_check(file_1) == self._sha256_check(file_2)
         
 
 
@@ -103,3 +162,12 @@ if __name__ == "__main__":
     parser = CLIParser()
     test = parser.load_config()
     print(test)
+
+    test2 = SynchronizeFiles(
+        test.sync_interval,
+        test.sync_numbers,
+        test.source_folder_path,
+        test.replica_folder_path
+    )
+
+    test2._check_and_sync()
